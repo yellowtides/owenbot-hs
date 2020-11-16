966d26fdaf955b5f25ee438daf226e93e99aa2b8
9nine9nine9 <s2026618@ed.ac.uk>
2020-11-16 17:26:20 +0000

    Wrote some basic funcs, included discord package

--- src/Lib.hs
+++ src/Lib.hs
@@ -1,6 +1,34 @@
+-- {-# LANGUAGE OverloadedStrings #-}
+
 module Lib
-    ( someFunc
+    ( isCommand
     ) where
 
-someFunc :: IO ()
-someFunc = putStrLn "someFunc"
+import Data.List (isPrefixOf)
+import Data.Char (toLower)
+
+import Hoogle
+
+cmds :: [String]
+cmds = ["thm", "theorem", "def", "definition", "lem", "lemma",
+        "hoogle", "doc"]
+
+lower = map toLower
+
+isCommand :: [String] -> String -> Bool
+isCommand cmds (c:str) = c == ':' && any (`isPrefixOf` lower (str++" ")) cmds
+
+isThm :: String -> Bool
+isThm    = isCommand ["thm", "theorem"]
+
+isDef :: String -> Bool
+isDef    = isCommand ["def", "definition"]
+
+isLem :: String -> Bool
+isLem    = isCommand ["lem", "lemma"]
+
+isHoogle :: String -> Bool
+isHoogle = isCommand ["hoogle"]
+
+isDoc:: String -> Bool
+isDoc    = isCommand ["doc"]
