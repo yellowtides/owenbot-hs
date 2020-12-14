ebee8b05ee65ae2bb2943b76dc23e73759a4f027
Andi <s2095623@ed.ac.uk>
2020-12-14 22:28:40 +0000

    Revert regex edit

--- src/OwenRegex.hs
+++ src/OwenRegex.hs
@@ -7,17 +7,17 @@ import qualified Data.Text as T
 import Text.RawString.QQ
 
 thmRE, defRE, lemmaRE, textbookRE, syllogismsRE, booleanRE, hoogleInfRE, helpRE :: T.Text
-thmRE        = [r|^:(thm|theorem)\s*([0-9]{1,2}\.[0-9]{1,2}\.[0-9]{1,2})\s*$|]   -- :thm
+thmRE        = ":(thm|theorem) *([0-9]{1,2}\\.[0-9]{1,2}\\.[0-9]{1,2})"         -- :thm
 defRE        = "^:(def|definition) *([0-9]{1,2}\\.[0-9]{1,2})"                  -- :def
 lemmaRE      = ":(lem|lemma) *([0-9]{1,2}\\.[0-9]{1,2}\\.[0-9]{1,2})"           -- :lemma
 textbookRE   = ":textbook *"                                                    -- :textbook
 syllogismsRE = ":(syllogisms|syl) *"                                            -- :syllogisms
 booleanRE    = ":(boolean|bool) *"                                              -- :boolean
 hoogleInfRE  = "^:doc [a-z']+"                                                  -- :doc
-helpRE       = [r|^:helpme\s*$|]                                                -- :help
+helpRE       = "^:helpme *"                                                     -- :help
 
 owoifiableRE :: T.Text
-owoifiableRE = [r|[lrLR]|[nNmM][oO]|]
+owoifiableRE  = "[lrLR]|[nNmM][oO]"
 
 commandREs :: [T.Text] -- list of all command regexes 
 commandREs = [thmRE, defRE, lemmaRE, textbookRE, syllogismsRE, booleanRE, hoogleInfRE, helpRE]
