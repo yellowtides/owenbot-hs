73e0e53865c2d970bfb238a2183e68679e2c817e
Andi <s2095623@ed.ac.uk>
2020-12-14 19:03:31 +0000

    Prettify CommandHandler

--- src/CommandHandler.hs
+++ src/CommandHandler.hs
@@ -21,25 +21,30 @@ isCommand m = any (m =~) commandREs
 
 handleCommand :: Message -> DiscordHandler (Either RestCallErrorCode Message)
 handleCommand m
-    | content =~ thmRE        = sendImage channel ("Theorem " <> T.pack (getVal content)) ("./src/assets/theorems/" ++  (getVal content))
-    | content =~ defRE        = sendImage channel ("Definition " <> T.pack (getVal content)) ("./src/assets/definitions/" ++  (getVal content))
-    | content =~ lemmaRE      = sendImage channel ("Lemma " <> T.pack (getVal content)) ("./src/assets/lemmas/" ++  (getVal content))
-    | content =~ textbookRE   = restCall(R.TriggerTypingIndicator channel) >> sendImage channel "Textbook.pdf" "./src/assets/textbook/nichol.pdf"
-    | content =~ syllogismsRE = sendImage channel "Id-smash-aristotle.png" "./src/assets/cl/syllogisms.png"
-    | content =~ booleanRE    = sendImage channel "literally-satan.png" "./src/assets/cl/Bool.png"
+    | content =~ thmRE        = sendFile channel ("Theorem "                  <> T.pack (getVal content)) 
+                                                 ("./src/assets/theorems/"    ++ getVal content)
+    | content =~ defRE        = sendFile channel ("Definition "               <> T.pack (getVal content)) 
+                                                 ("./src/assets/definitions/" ++ getVal content)
+    | content =~ lemmaRE      = sendFile channel ("Lemma "                    <> T.pack (getVal content)) 
+                                                 ("./src/assets/lemmas/"      ++ getVal content)
+    | content =~ textbookRE   = simTyping $ 
+                                sendFile channel "Textbook.pdf"               "./src/assets/textbook/nichol.pdf"
+    | content =~ syllogismsRE = sendFile channel "Id-smash-aristotle.png"     "./src/assets/cl/syllogisms.png"
+    | content =~ booleanRE    = sendFile channel "literally-satan.png"        "./src/assets/cl/Bool.png"
     | content =~ hoogleInfRE  = sendMessage channel "test"
     | content =~ helpRE       = sendMessage channel "test"
     where
-        content = messageText m
-        channel = messageChannel m
-            
+        content   = messageText m
+        channel   = messageChannel m
+        simTyping = (>>) $ restCall (R.TriggerTypingIndicator channel)
+
 sendMessage :: ChannelId -> T.Text -> DiscordHandler (Either RestCallErrorCode Message)
 sendMessage c xs = restCall (R.CreateMessage c xs)
 
-sendImage :: ChannelId -> T.Text -> FilePath -> DiscordHandler (Either RestCallErrorCode Message)
-sendImage c t f 
+sendFile :: ChannelId -> T.Text -> FilePath -> DiscordHandler (Either RestCallErrorCode Message)
+sendFile c t f 
     | fileContent == Nothing = sendMessage c "iw cannow be foun uwu"
-    | otherwise              = restCall (R.CreateMessageUploadFile c t . fromJust $ unsafeFileOpen f)
+    | otherwise              = restCall (R.CreateMessageUploadFile c t . fromJust $ fileContent)
     where
         fileContent = unsafeFileOpen f
 
