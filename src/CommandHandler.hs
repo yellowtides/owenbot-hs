a50d1f42dbfc9a213083284056e749a9573c042d
Artemis Livingstone <s2052619@ed.ac.uk>
2020-12-14 18:10:07 +0000

    Fixed file upload

--- src/CommandHandler.hs
+++ src/CommandHandler.hs
@@ -9,7 +9,7 @@ import System.IO.Unsafe ( unsafePerformIO )
 import qualified Data.ByteString as B
 import qualified Data.Text as T
 import Text.Regex.TDFA ( (=~) )
-
+import Data.Char ( isAlpha, isSpace )
 import OwenRegex
 
 -- map through all the regexes and see if any of them match
@@ -18,20 +18,20 @@ isCommand m = any (m =~) commandREs
 
 handleCommand :: Message -> DiscordHandler (Either RestCallErrorCode Message)
 handleCommand m
-    | content =~ thmRE        = sendMessage channel
-    | content =~ defRE        = sendMessage channel
-    | content =~ lemmaRE      = sendMessage channel
-    | content =~ textbookRE   = sendImage channel "Textbook.pdf" "./assets/cl/INF1A-2020-11-30.pdf"
-    | content =~ syllogismsRE = sendImage channel "Id-smash-aristotle.png" "./assets/cl/syllogisms.png"
-    | content =~ booleanRE    = sendMessage channel
-    | content =~ hoogleInfRE  = sendMessage channel
-    | content =~ helpRE       = sendMessage channel
+    | content =~ thmRE        = sendImage channel ("Theorem " <> T.pack (getVal content)) ("./src/assets/theorems/" ++  (getVal content))
+    | content =~ defRE        = sendImage channel ("Definition " <> T.pack (getVal content)) ("./src/assets/definitions/" ++  (getVal content))
+    | content =~ lemmaRE      = sendImage channel ("Lemma " <> T.pack (getVal content)) ("./src/assets/lemmas/" ++  (getVal content))
+    | content =~ textbookRE   = restCall(R.TriggerTypingIndicator channel) >> sendImage channel "Textbook.pdf" "./src/assets/textbook/nichol.pdf"
+    | content =~ syllogismsRE = sendImage channel "Id-smash-aristotle.png" "./src/assets/cl/syllogisms.png"
+    | content =~ booleanRE    = sendImage channel "literally-satan.png" "./src/assets/cl/Bool.png"
+    | content =~ hoogleInfRE  = sendMessage channel "test"
+    | content =~ helpRE       = sendMessage channel "test"
     where
         content = messageText m
         channel = messageChannel m
             
-sendMessage :: ChannelId -> DiscordHandler (Either RestCallErrorCode Message)
-sendMessage c = restCall (R.CreateMessage c "Message received")
+sendMessage :: ChannelId -> T.Text -> DiscordHandler (Either RestCallErrorCode Message)
+sendMessage c xs = restCall (R.CreateMessage c xs)
 
 
 sendImage :: ChannelId -> T.Text -> FilePath -> DiscordHandler (Either RestCallErrorCode Message)
@@ -40,3 +40,6 @@ sendImage c t f= do
 
 unsafeFileOpen :: FilePath -> B.ByteString
 unsafeFileOpen f = unsafePerformIO $ B.readFile f
+
+getVal :: T.Text -> String
+getVal xs= (dropWhile (\x -> isAlpha x||isSpace x) $drop 1 $ T.unpack xs ) ++ ".png"
