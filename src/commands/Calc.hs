6d6f66ea363bf60d949736b918b9134d1b3453e5
Artemis Livingstone <s2052619@ed.ac.uk>
2021-01-06 22:58:15 +0000

    updated link

--- src/commands/Calc.hs
+++ src/commands/Calc.hs
@@ -7,4 +7,5 @@ import Discord ( DiscordHandler, RestCallErrorCode )
 import Utils (sendMessageChan, sendFileChan)
 
 sendTextbookChan :: ChannelId -> DiscordHandler (Either RestCallErrorCode Message)
-sendTextbookChan chan = sendMessageChan chan "The textbook can be found here: \n http://111.90.145.72/get.php?md5=13ecb7a2ed943dcb4a302080d2d8e6ea&key=WJUIYMKD48JZUQX2&mirr=1"
+sendTextbookChan chan = sendMessageChan chan "The textbook can be found here: \n http://libgen.gs/ads.php?md5=13ecb7a2ed943dcb4a302080d2d8e6ea"
+-- sendTextbookChan chan = sendFileChan chan "Bonkulus.pdf" "./src/assets/textbooks/Calc-pdf.pdf"
