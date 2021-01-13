6e51126c481c4c610dea989f912e166a3a4cebad
Artemis Livingstone <s2052619@ed.ac.uk>
2021-01-13 14:41:09 +0000

    added limit customisation

--- src/commands/ReactHandler.hs
+++ src/commands/ReactHandler.hs
@@ -3,31 +3,34 @@ module ReactHandler where
 
 import qualified Discord.Requests as R
 import Data.Text as T (pack, toUpper, Text)
+import Data.Functor ((<&>))
 import Discord ( restCall, DiscordHandler, RestCallErrorCode )
 import Discord.Types
     ( Snowflake,
       Attachment(attachmentUrl),
       Emoji(emojiName),
       Message(messageReactions, messageId, messageText, messageChannel,
-              messageAttachments),
+              messageAttachments, messageChannel),
       MessageReaction(messageReactionCount, messageReactionEmoji),
       CreateEmbed(CreateEmbed),
       CreateEmbedImage(CreateEmbedImageUrl),
       ReactionInfo(reactionEmoji, reactionChannelId, reactionMessageId) )
 import MiscHandler ()
 import Utils
-    ( pingAuthorOf,
+    (sendMessageChan, isRole,  pingAuthorOf,
       linkChannel,
       getMessageLink,
       sendMessageChanEmbed,
       getTimestampFromMessage,
       openCSV,
-      addToCSV ) 
+      addToCSV,
+      rmFuncText ) 
 import UnliftIO(liftIO)
+import Control.Monad (guard)
 
 
 hallOfFameEmotes :: [T.Text]
-hallOfFameEmotes = ["XREE", "KEKW"] --capitalise emotes
+hallOfFameEmotes = ["XREE", "KEKW", "STAR"] --capitalised emotes
 
 hallOfFameChannel :: Snowflake
 hallOfFameChannel = 790936269382615082 --the channel id for the hall of fame
@@ -47,7 +50,8 @@ isEligibleForHallOfFame r = do
   case messM of
     Right mess -> do
       msgIdlist <- liftIO $ openCSV "fame.csv"
-      pure $ Right $ any (\x -> (messageReactionCount x >= 4) && (toUpper (emojiName (messageReactionEmoji x)) `elem` hallOfFameEmotes) && (show (messageId mess) `notElem` msgIdlist)) (messageReactions mess)
+      limit <- liftIO readLimit
+      pure $ Right $ any (\x -> (messageReactionCount x >= limit) && (toUpper (emojiName (messageReactionEmoji x)) `elem` hallOfFameEmotes) && (show (messageId mess) `notElem` msgIdlist)) (messageReactions mess)
     Left err -> pure $ Left err
 
 handleHallOfFame :: ReactionInfo -> DiscordHandler (Either RestCallErrorCode Message)
@@ -77,3 +81,20 @@ getHallOfFameEmbed m = do
   case messLinkM of
     Right messLink -> pure $ Right (CreateEmbed "" "" Nothing "👑 best of ouw buwwshit" "" Nothing ((getHallOfFameDescription m) <> "\n\n[Original Message](" <> messLink <> ")") [] (Just (CreateEmbedImageUrl $ getImageFromMessage m)) (getTimestampFromMessage m) Nothing)
     Left err -> pure $ Left err
+
+-- setLimit :: Message -> Int -> IO()
+-- setLimit :: Message -> Int -> DiscordHandler ()
+setLimit :: Message -> Int -> DiscordHandler (Either RestCallErrorCode Message)
+setLimit m i = do 
+  b1 <- isRole m "OwenDev"
+  if b1 
+    then do 
+      liftIO $ editLimit i
+      sendMessageChan (messageChannel m) "New Limit Set"
+    else sendMessageChan (messageChannel m ) "Insufficient Priveledges"
+
+editLimit :: Int -> IO()
+editLimit = writeFile "src/config/reactLim.conf" . show 
+
+readLimit :: IO Int
+readLimit = openCSV "src/config/reactLim.conf" <&> (read . head)
