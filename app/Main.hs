88e35eabb3d12cfd7ddc7b65dd8820b2805f912d
Artemis Livingstone <s2052619@ed.ac.uk>
2021-01-21 11:03:43 +0000

    added startup message

--- app/Main.hs
+++ app/Main.hs
@@ -4,18 +4,33 @@ module Main where
 
 import qualified Data.Text as T
 import qualified Data.Text.IO as TIO
-
+import Discord.Requests as R
+import Control.Monad (forM_, when)
 import Discord
-
 import EventHandler (handleEvent)
+
+import qualified Discord.Requests as R
+import Discord.Types
+
 
 -- | UWU
 owen :: T.Text -> IO ()
 owen t = do
     userFacingError <- runDiscord $ def { discordToken   = t
+                                        , discordOnStart = startHandler
                                         , discordOnEvent = handleEvent
                                         , discordOnLog = \s -> TIO.putStrLn s >> TIO.putStrLn "" }
     TIO.putStrLn userFacingError
+
+startHandler :: DiscordHandler ()
+startHandler = do
+    let chan = 801763198792368129 ::ChannelId --this channel is the bot-start channel on the test server, change to point towards your channel.
+    _ <- restCall $ R.CreateMessage chan "@here Hewwo, I am bawck! UwU"
+    pure ()
+
+isTextChannel :: Channel -> Bool
+isTextChannel ChannelText {} = True
+isTextChannel _ = False
 
 main :: IO ()
 main = do
