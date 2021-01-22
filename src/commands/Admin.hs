a3c677863badb16f1e0545061081a9119b9c8975
Artemis Livingstone <s2052619@ed.ac.uk>
2021-01-22 15:19:14 +0000

    added local, remote and diff functionality

--- src/commands/Admin.hs
+++ src/commands/Admin.hs
@@ -1,7 +1,7 @@
 {-# LANGUAGE OverloadedStrings #-}
 
-module Admin (sendInstanceChan) where
-
+module Admin (sendInstanceChan, gitLocal, gitRemote, commitsAhead) where
+import Data.Text as T
 import Discord.Types ( ChannelId, Message(messageChannel) )
 import Discord ( DiscordHandler, RestCallErrorCode )
 import Utils (sendMessageChan, isRole, captureCommandOutput)
@@ -12,6 +12,15 @@ sendInstanceChan m = do
   b1 <- isRole m "OwenDev"
   if b1
     then do
-      output <- liftIO $ captureCommandOutput "git" ["rev-parse", "HEAD"]
-      sendMessageChan (messageChannel m) ("Instance running on git commit: " <> output)
+      loc <- liftIO gitLocal
+      remote <- liftIO gitRemote
+      commits <- liftIO commitsAhead
+      sendMessageChan (messageChannel m) ("Instance running on git commit: " <> loc <> 
+                                          "\nRemote at: " <> remote <>
+                                          "\nRemote is " <> commits <> " commits ahead")
     else sendMessageChan (messageChannel m) "Insufficient privileges."
+
+gitLocal, gitRemote, commitsAhead :: IO T.Text 
+gitLocal = captureCommandOutput "git" ["rev-parse", "HEAD"]
+gitRemote = captureCommandOutput "git" ["rev-parse", "origin/main"]
+commitsAhead = captureCommandOutput "git" ["rev-list", "--count", "HEAD", "^origin/main"]
