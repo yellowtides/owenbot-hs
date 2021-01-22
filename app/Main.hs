35e3e0b50f41ecd26643759cfdda0075f73b65b4
Artemis Livingstone <s2052619@ed.ac.uk>
2021-01-22 16:06:29 +0000

    added instance info
    Instance info is now displayed on boot and with command

--- app/Main.hs
+++ app/Main.hs
@@ -4,14 +4,18 @@ module Main where
 
 import qualified Data.Text as T
 import qualified Data.Text.IO as TIO
+import Control.Monad 
 import Discord.Requests as R
-import Control.Monad (forM_, when)
 import Discord
+    ( restCall,
+      runDiscord,
+      def,
+      DiscordHandler,
+      RunDiscordOpts(discordToken, discordOnStart, discordOnEvent,
+                     discordOnLog) )
+import Discord.Types ( ChannelId, Channel(ChannelText) )
 import EventHandler (handleEvent)
-
-import qualified Discord.Requests as R
-import Discord.Types
-
+import Admin (sendInstanceInfo)
 
 -- | UWU
 owen :: T.Text -> IO ()
@@ -26,6 +30,7 @@ startHandler :: DiscordHandler ()
 startHandler = do
     let chan = 801763198792368129 ::ChannelId --this channel is the bot-start channel on the test server, change to point towards your channel.
     _ <- restCall $ R.CreateMessage chan "@here Hewwo, I am bawck! UwU"
+    _ <- sendInstanceInfo chan
     pure ()
 
 isTextChannel :: Channel -> Bool
--- src/commands/Admin.hs
+++ src/commands/Admin.hs
@@ -1,26 +1,36 @@
 {-# LANGUAGE OverloadedStrings #-}
 
-module Admin (sendInstanceChan, gitLocal, gitRemote, commitsAhead) where
+module Admin (sendInstanceChan, gitLocal, gitRemote, commitsAhead, sendInstanceInfo) where
 import Data.Text as T
-import Discord.Types ( ChannelId, Message(messageChannel) )
+import Discord.Types (ChannelId, Message(messageChannel) )
 import Discord ( DiscordHandler, RestCallErrorCode )
 import Utils (sendMessageChan, isRole, captureCommandOutput)
 import UnliftIO(liftIO)
+import Discord.Internal.Types (Channel(channelId))
+import Data.Char (isSpace)
+
+rstrip :: Text -> Text
+rstrip = T.reverse . T.dropWhile isSpace . T.reverse
 
 sendInstanceChan :: Message -> DiscordHandler (Either RestCallErrorCode Message)
 sendInstanceChan m = do 
   b1 <- isRole m "OwenDev"
   if b1
-    then do
-      loc <- liftIO gitLocal
-      remote <- liftIO gitRemote
-      commits <- liftIO commitsAhead
-      sendMessageChan (messageChannel m) ("Instance running on git commit: " <> loc <> 
-                                          "\nRemote at: " <> remote <>
-                                          "\nRemote is " <> commits <> " commits ahead")
+    then
+      sendInstanceInfo $ messageChannel m
     else sendMessageChan (messageChannel m) "Insufficient privileges."
 
 gitLocal, gitRemote, commitsAhead :: IO T.Text 
 gitLocal = captureCommandOutput "git" ["rev-parse", "HEAD"]
 gitRemote = captureCommandOutput "git" ["rev-parse", "origin/main"]
 commitsAhead = captureCommandOutput "git" ["rev-list", "--count", "HEAD", "^origin/main"]
+
+sendInstanceInfo :: ChannelId  -> DiscordHandler (Either RestCallErrorCode Message)
+sendInstanceInfo chan = do
+  loc <- liftIO gitLocal
+  remote <- liftIO gitRemote
+  commits <- liftIO commitsAhead
+  sendMessageChan chan ("Instance Info: \n" <>
+                        "Local at: " <> loc <>  --as all things returned by captureCommandOuput has a newline at the end
+                        "Remote at: " <> remote <>
+                        "Remote is " <> rstrip commits <> " commits ahead")
