a49aa1bf9f72d313402691fc2d2e92fab9b05973
Jacob Walters <J.Walters-6@sms.ed.ac.uk>
2021-02-02 19:35:45 +0000

    add margaret thatcher detection and ~~removal~~ handling

--- src/MiscHandler.hs
+++ src/MiscHandler.hs
@@ -32,6 +32,12 @@ isNietzsche = (=~= ("[gG]od *[iI]s *[dD]ead" :: T.Text))
 handleNietzsche :: Message -> DiscordHandler (Either RestCallErrorCode Message)
 handleNietzsche m = liftIO (TIO.readFile "./src/assets/nietzsche.txt") >>= sendMessageChan (messageChannel m) . owoify
 
+isThatcher :: T.Text -> Bool
+isThatcher = (=~= ("[tT]atcher *[iI]s *[dD]ead" :: T.Text))
+
+handleThatcher :: Message -> DiscordHandler (Either RestCallErrorCode Message)
+handleThatcher m = sendMessageChan (messageChannel m) "https://www.youtube.com/watch?v=lKnveREe31g"
+
 isDadJoke :: T.Text -> Maybe T.Text
 isDadJoke t = case captures of
                   [] -> Nothing
