c6619ae3ac808c96cd8648f8f8e67b968e9eae43
Yuto Takano <moa17stock@gmail.com>
2021-03-02 15:51:21 +0000

    Make Status a bit cleaner

--- src/Status.hs
+++ src/Status.hs
@@ -3,48 +3,48 @@ module Status where
 
 import qualified Data.Text as T
 import qualified Data.Text.IO as TIO
-import Data.Functor ((<&>))
-import Discord.Internal.Types.Gateway
-import Discord (
-    sendCommand,
-    DiscordHandler )
-import UnliftIO(liftIO)
-import Utils (openCSV)
+import           Data.Functor           ( (<&>) )
+import           Discord.Internal.Types.Gateway
+import           Discord                ( sendCommand
+                                        , DiscordHandler
+                                        )
+import           UnliftIO               ( liftIO )
+import           Utils                  ( openCSV )
 
 -- | Convert intuitive strings into the respective DataTypes
 -- Passes values onto updateStatus'
 -- Although we revert to defaults if enums don't match, the caller of this function
 -- should always check first on their own and provide approriate error messages.
 updateStatus :: T.Text -> T.Text -> T.Text -> DiscordHandler ()
-updateStatus newStatus newType newName =
-    let
-        newStatusParsed = case newStatus of
-            "online" -> UpdateStatusOnline
-            "dnd" -> UpdateStatusDoNotDisturb
-            "idle" -> UpdateStatusAwayFromKeyboard
-            "invisible" -> UpdateStatusInvisibleOffline
-            _ -> UpdateStatusOnline -- revert to online if not match
-        newTypeParsed = case newType of
-            "playing" -> ActivityTypeGame
-            "streaming" -> ActivityTypeStreaming
-            "listening" -> ActivityTypeListening
-            "competing" -> ActivityTypeCompeting
-            _ -> ActivityTypeGame -- revert to playing if not match
-     in do
-        updateStatus' newStatusParsed newTypeParsed newName
+updateStatus newStatus newType newName = 
+    updateStatus' newStatusParsed newTypeParsed newName
+  where
+    newStatusParsed = case newStatus of
+        "online" -> UpdateStatusOnline
+        "dnd" -> UpdateStatusDoNotDisturb
+        "idle" -> UpdateStatusAwayFromKeyboard
+        "invisible" -> UpdateStatusInvisibleOffline
+        _ -> UpdateStatusOnline -- revert to online if not match
+    newTypeParsed = case newType of
+        "playing" -> ActivityTypeGame
+        "streaming" -> ActivityTypeStreaming
+        "listening" -> ActivityTypeListening
+        "competing" -> ActivityTypeCompeting
+        _ -> ActivityTypeGame -- revert to playing if not match    
 
 -- | Sets the Discord status
 updateStatus' :: UpdateStatusType -> ActivityType -> T.Text -> DiscordHandler ()
-updateStatus' newStatus newType newName = sendCommand (UpdateStatus (UpdateStatusOpts {
-    updateStatusOptsSince = Nothing,
-    updateStatusOptsGame = Just (Activity {
-        activityName = newName,
-        activityType = newType,
-        activityUrl = Nothing
-        }),
-    updateStatusOptsNewStatus = newStatus,
-    updateStatusOptsAFK = False 
-    }))
+updateStatus' newStatus newType newName = sendCommand $ 
+    UpdateStatus (UpdateStatusOpts {
+        updateStatusOptsSince = Nothing,
+        updateStatusOptsGame = Just (Activity {
+            activityName = newName,
+            activityType = newType,
+            activityUrl = Nothing
+            }),
+        updateStatusOptsNewStatus = newStatus,
+        updateStatusOptsAFK = False 
+        })
 
 -- | Sets the status from file on bot launch
 -- Should be called only once.
@@ -55,7 +55,7 @@ setStatusFromFile = do
     updateStatus
         (head parts)
         ((head.tail) parts)
-        (T.unwords $ (tail.tail) parts)
+        (T.unwords $ (tail . tail) parts)
 
 editStatusFile :: T.Text -> T.Text -> T.Text -> IO ()
 editStatusFile newStatus newType newName = 
