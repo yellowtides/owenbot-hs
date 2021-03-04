e0c5c42eedb56454e4d897c887334d97835bd7e8
Yuto Takano <moa17stock@gmail.com>
2021-03-04 16:05:39 +0000

    Add quickCheck tests for the CSV module

--- test/Spec.hs
+++ test/Spec.hs
@@ -1,2 +1,19 @@
+module Main where
+
+import Test.QuickCheck                  ( quickCheck
+                                        , Property
+                                        , (==>)
+                                        )
+import Test.QuickCheck.Monadic          ( assert
+                                        , monadicIO
+                                        )
+import TestCSV                          ( prop_readWriteCSV
+                                        , prop_readWriteSingleColCSV
+                                        , cleanupTempCSV
+                                        )
+
 main :: IO ()
-main = putStrLn "Test suite not yet implemented"
+main = do
+    quickCheck prop_readWriteCSV
+    quickCheck prop_readWriteSingleColCSV
+    cleanupTempCSV
