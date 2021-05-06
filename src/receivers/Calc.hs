82abf65afc94e55fcbade22d20aef5d3fa346172
9nine9nine9 <s2026618@ed.ac.uk>
2021-05-06 01:46:26 +0100

    Abstract out URL for textbook

--- src/receivers/Calc.hs
+++ src/receivers/Calc.hs
@@ -16,8 +16,10 @@ receivers = [ sendTextbook ]
 calctextbookRE :: T.Text
 calctextbookRE = textbookRE <> "calc"
 
+url :: T.Text
+url = "http://gen.lib.rus.ec/book/index.php?md5=13ecb7a2ed943dcb4a302080d2d8e6ea"
+
 sendTextbook :: Message -> DiscordHandler ()
 sendTextbook m = newCommand m calctextbookRE $ \_ ->
     sendMessageChan (messageChannel m)
-        "The textbook can be found here:\nhttp://gen.lib.rus.ec/book/index.php?md5=13ecb7a2ed943dcb4a302080d2d8e6ea"
-    -- sendTextbookChan chan = sendFileChan chan "Bonkulus.pdf" "./src/assets/textbooks/Calc-pdf.pdf"
+        $ "The textbook can be found here:\n" <> url
