a7df1cac06f429533e786bed9b2c6b52d21cd900
Paula <s2036366@ed.ac.uk>
2021-05-09 22:38:37 +0100

    i documented the owofy page

--- src/Owoifier.hs
+++ src/Owoifier.hs
@@ -16,17 +16,21 @@ owolessDelim = "```"
 alternate :: (a -> a) -> [a] -> [a]
 alternate f = zipWith ($) (cycle [f, id])
 
+-- | This takes a text and returns an owofied text
+-- e.g. "North ```West```" -> "Nyowth ```West``` owo"
 owoify :: T.Text -> T.Text
 owoify text = T.intercalate owolessDelim (alternate owoifySegment segments) <> " owo"
     where segments = T.splitOn owolessDelim text
 
+-- | Implements the weakOwoify over the segments of text
 owoifySegment :: T.Text -> T.Text
 owoifySegment = doInserts . weakOwoify
 
+-- | Maps the function that actually owofies the text over each character of the string
 weakOwoify :: T.Text -> T.Text
 weakOwoify = T.map owoifyChar
 
--- returns ['l'/'L'/'r'/'R'] for ['w'/'W'], otherwise `id`
+-- | Substitutes given characters for owoified variants in a string, otherwise `id`
 owoifyChar :: Char -> Char
 owoifyChar c
     | c `elem` ("LR" :: [Char]) = 'W'
@@ -34,31 +38,37 @@ owoifyChar c
     | otherwise                 = c
 
 -- | Takes the cartesian product of two lists of chars and packs as a Text
+-- e.g. "nmNM <-> "o" returns ["no", "mo", "No", "Mo"]
 (<->) :: [Char] -> [Char] -> [T.Text]
 (<->) as bs = [T.pack [a,b] | a <- as, b <- bs]
 
+-- | Checks if the y added should be lowercase
 segmentsSmallY :: [T.Text]
 segmentsSmallY = "NMnm" <-> "o" ++ "nm" <-> "O"
 
+-- | Checks if the y added should be uppercase
 segmentsBigY :: [T.Text]
 segmentsBigY = "NM" <-> "O"
 
+-- | Inserts the wanted symbol in between segments
 mkRules :: [(Char, [T.Text])] -> [(T.Text, Char)]
 mkRules defs = do
     (sym, segments) <- defs
     (, sym) <$> segments
 
+-- | Depending on what type of segment it is, it inserts a Y or y
 insertionRules :: [(T.Text, Char)]
 insertionRules = mkRules
     [ ('y', segmentsSmallY)
     , ('Y', segmentsBigY)
     ]
-
+-- | If it the word comes as a tuple with a Y then it is interpersed in the word and then replaces the og
 applyRule :: (T.Text, Char) -> T.Text -> T.Text
 applyRule rule
     = let (word, ins) = rule
           newWord     = T.intersperse ins word in
     T.replace word newWord
 
+-- | Inserts the y-words in the text
 doInserts :: T.Text -> T.Text
 doInserts str = foldl (flip applyRule) str insertionRules
