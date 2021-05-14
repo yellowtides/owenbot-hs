bba8f6e8355024b83df57c328d841b239e22e536
9nine9nine9 <s2026618@ed.ac.uk>
2021-05-14 17:15:15 +0100

    Simplify pointfree, fix codeblocks when ` is in msg

--- src/receivers/Haskell.hs
+++ src/receivers/Haskell.hs
@@ -7,25 +7,28 @@ module Haskell (
 import           Data.Maybe              ( fromMaybe )
 import           Discord                 ( DiscordHandler
                                          )
-import           Discord.Types ( Message ( messageChannel ) )
+import           Discord.Types           ( Message )
 import           Pointfree               ( pointfree' )
 import qualified Data.Text as T
 
-import Owoifier                ( owoify )
-import Utils                   ( sendMessageChan
-                               , newCommand )
+import           Utils                   ( respond
+                                         , newCommand )
 
 receivers =
     [ pointfree
     ]
 
+-- | Surrounds a String with backticks for nice formatting on Discord
 codeblock :: String -> String
-codeblock = ("`" ++) . (++ "`")
+codeblock = ("``" ++) . (++ "``")
 
+-- | Converts given haskell code to point-free form
+-- >>> :pf f x = 1 + x
+-- f = (1 +)
+--
+-- >>> :pf codeblock str = "`" ++ str ++ "`"
+-- codeblock = ("``" ++) . (++ "``")
 pointfree :: Message -> DiscordHandler ()
-pointfree m = newCommand m "pf (.*)" $ \captures -> do
-    case captures of
-         [hs] -> sendMessageChan (messageChannel m) $ pf hs
-         _    -> sendMessageChan (messageChannel m) $ owoify
-                    "Usage: `:pf <haskell code>`"
+pointfree m = newCommand m "pf (.*)"
+        $ \captures -> respond m $ pf $ head captures
     where pf = T.pack . codeblock . fromMaybe "" . pointfree' . T.unpack
