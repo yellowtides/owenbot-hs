d11d0e847bf21abe2f0a60dbd1907330cfad4065
Yuto Takano <moa17stock@gmail.com>
2021-05-24 17:30:16 +0100

    Implement successfully parsing [T.Text]

--- src/Command/Parser.hs
+++ src/Command/Parser.hs
@@ -27,7 +27,7 @@ import           Text.Parsec
 import           Discord.Types
 
 -- | @manyTill1 p end@ is a parser that applies parser @p@ /one/ or more times
--- until parser @end@ succeeds. This is a variation on @manyTill@ from Parsec.
+-- until parser @end@ succeeds. This is a variation on 'manyTill' from Parsec.
 manyTill1 :: (Stream s m t) => ParsecT s u m a -> ParsecT s u m end -> ParsecT s u m [a]
 manyTill1 p end = do { x <- p; xs <- manyTill p end; return (x:xs) }
 
@@ -39,29 +39,54 @@ class ParsableArgument a where
     -- and the remaining input.
     parserForArg :: Message -> T.Parser (a, T.Text)
 
+-- | The message that invoked the command.
 instance ParsableArgument Message where
     parserForArg msg = do
         remaining <- getInput
         pure (msg, remaining)
 
+-- | Any number of non-space characters. If quoted, spaces are allowed.
+-- Quotes in quoted text can be escaped with a backslash. The following is
+-- parsed as a single text: 
+-- @\"He said, \\\"Lovely\\\".\"@
 instance ParsableArgument T.Text where
     parserForArg msg =
-        (flip label) "any text" $ do
-            -- consume at least one character that is not a space or eof
-            parsed <- manyTill1 anyChar (void (lookAhead space) <|> eof)
+        (flip label) "word or a quoted phrase" $ do
+            -- try quoted text first. if it failed, then normal word
+            parsed <- quotedText <|> word
             -- consume end of input or one or more spaces
             (eof <|> void (many1 space))
-            -- return remaining together with consume value
+            -- return remaining together with consumed value
             remaining <- getInput
             pure (T.pack parsed, remaining)
+      where
+        quotedText = do
+            -- consume quotes
+            char '"'
+            -- consume everything but quotes, unless it is escaped
+            content <- many $ noneOf "\"" <|> try (string "\\\"" >> return '"')
+            -- consume closing quote
+            char '"'
+            return content
+        word =
+            -- consume at least one character that is not a space or eof
+            manyTill1 anyChar (void (lookAhead space) <|> eof)
 
+-- | Zero or more texts. Each one could be quoted or not.
 instance ParsableArgument [T.Text] where
     parserForArg msg = do
-        -- consume at least one of any character, separated by spaces
-        rest <- (many1 anyChar) `sepBy` space
-        remaining <- getInput
-        pure (T.pack <$> rest, remaining)
+        (word, remaining) <- parserForArg msg :: T.Parser (T.Text, T.Text)
+        if T.length remaining > 0
+            then do
+                (rest, _) <- parserForArg msg :: T.Parser ([T.Text], T.Text)
+                pure (word:rest, "")
+            else 
+                pure ([word], remaining)
+        -- rest <- (many1 anyChar) `sepBy` space
+        -- remaining <- getInput
+        -- pure (T.pack <$> rest, remaining)
 
+-- | Parses "online" "dnd" "idle" and "invisible" as 'UpdateStatusType's
 instance ParsableArgument UpdateStatusType where
     parserForArg msg = do
         -- consume either of the following:
@@ -76,6 +101,8 @@ instance ParsableArgument UpdateStatusType where
         remaining <- getInput
         pure (parsed, remaining)
 
+-- | Parses "playing", "streaming", "listening to" and "competing in" as
+-- 'ActivityType's.
 instance ParsableArgument ActivityType where
     parserForArg msg = do
         -- consume either of the following:
