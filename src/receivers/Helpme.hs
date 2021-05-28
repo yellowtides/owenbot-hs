727ecc088e74629b700bf2069557da8137f24c41
Yuto Takano <moa17stock@gmail.com>
2021-05-28 18:11:05 +0100

    Testing `requires` by adding channel restriction on helpme

--- src/receivers/Helpme.hs
+++ src/receivers/Helpme.hs
@@ -2,11 +2,10 @@
 
 module Helpme ( receivers ) where
 
-import           Discord.Types      ( Message ( messageAuthor )
-                                    , User ( userId )
-                                    )
+import           Discord.Types
 import           Discord            ( DiscordHandler )
 import qualified Data.Text.IO as TIO
+import qualified Data.Text as T
 import           UnliftIO           ( liftIO )
 
 import           Owoifier           ( owoify )
@@ -19,7 +18,13 @@ receivers = [ sendHelpDM ]
 -- sendHelpDM :: (MonadDiscord m) => Command m (Message -> m ())
 sendHelpDM
     = runCommand
-    . help "Ahahaha"
+    . requires (channelRequirement "801763198792368129")
     . command "helpme" $ \m -> do
         liftIO (TIO.readFile "./src/assets/help.txt")
             >>= sendMessageDM (userId $ messageAuthor m) . owoify
+
+channelRequirement :: (MonadDiscord m) => String -> Message -> m (Maybe T.Text)
+channelRequirement cid msg = if (messageChannel msg) == (read cid)
+    then pure Nothing
+    else pure $ Just "need to be in channel"
+
