660f9055e691cfe7c75604e805d56b7ef6e4a093
9nine9nine9 <J.Walters-6@sms.ed.ac.uk>
2021-06-25 01:13:32 +0100

    Prepend 🎱 to 8ball responses

--- src/receivers/Misc.hs
+++ src/receivers/Misc.hs
@@ -197,29 +197,33 @@ changePronouns = do
 -- | List of magic 8-ball responses
 -- (from https://en.wikipedia.org/wiki/Magic_8-Ball#Possible_answers)
 ballAnswers :: [T.Text]
-ballAnswers = [ "It is Certain."
-                , "It is decidedly so."
-                , "Without a doubt."
-                , "Yes definitely."
-                , "You may rely on it."
-                , "As I see it, yes."
-                , "Most likely."
-                , "Outlook good."
-                , "Yes."
-                , "Signs point to yes."
+ballAnswers = map ("🎱 " `T.append`)
+    -- Positive responses
+    [ "It is Certain."
+    , "It is decidedly so."
+    , "Without a doubt."
+    , "Yes definitely."
+    , "You may rely on it."
+    , "As I see it, yes."
+    , "Most likely."
+    , "Outlook good."
+    , "Yes."
+    , "Signs point to yes."
 
-                , "Reply hazy, try again."
-                , "Ask again later."
-                , "Better not tell you now."
-                , "Cannot predict now."
-                , "Concentrate and ask again."
+    -- Non-committal responses
+    , "Reply hazy, try again."
+    , "Ask again later."
+    , "Better not tell you now."
+    , "Cannot predict now."
+    , "Concentrate and ask again."
 
-                , "Don't count on it."
-                , "My reply is no."
-                , "My sources say no."
-                , "Outlook not so good."
-                , "Very doubtful. "
-                ]
+    -- Negative responses
+    , "Don't count on it."
+    , "My reply is no."
+    , "My sources say no."
+    , "Outlook not so good."
+    , "Very doubtful. "
+    ]
 
 magic8ball :: (MonadDiscord m, MonadIO m) => Command m
 magic8ball = regexCommand "^:8ball.*" $ \m _ -> liftIO (select ballAnswers) >>= respond m
