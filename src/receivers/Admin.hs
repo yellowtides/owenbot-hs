c4707374393a31d546d3f924ba6e78f17ac6709d
9nine9nine9 <J.Walters-6@sms.ed.ac.uk>
2021-06-28 16:24:34 +0100

    Add more docs & help messages; drop obsolete funcs

--- src/receivers/Admin.hs
+++ src/receivers/Admin.hs
@@ -73,15 +73,11 @@ gitLocal = do
 commitsAhead :: IO T.Text
 commitsAhead = captureCommandOutput "git rev-list --count HEAD..origin/main"
 
+-- | Checks if the bot is running inside of a git repo
 isGitRepo :: IO Bool
 isGitRepo = doesPathExist ".git"
 
-sendGitInfo :: Command DiscordHandler
-sendGitInfo
-    = requires devPerms
-    . command "repo" $ \m ->
-        sendGitInfoChan $ messageChannel m
-
+-- | Sends the git info to a specific channel
 sendGitInfoChan :: (MonadDiscord m, MonadIO m) => ChannelId -> m ()
 sendGitInfoChan chan = do
     inRepo <- liftIO isGitRepo
@@ -97,12 +93,14 @@ sendGitInfoChan chan = do
             x   -> sendMessageChan chan $ "*Git: Upstream is " <> x <>
                 " commits ahead. Current local HEAD is:* \n" <> localStatus
 
-sendInstanceInfo :: Command DiscordHandler
-sendInstanceInfo
+-- | Responds to a message with the git info
+sendGitInfo :: Command DiscordHandler
+sendGitInfo
     = requires devPerms
-    . command "instance" $ \m ->
-        sendInstanceInfoChan $ messageChannel m
+    . command "repo" $ \m ->
+        sendGitInfoChan $ messageChannel m
 
+-- | Sends process instance info to a given channel
 sendInstanceInfoChan :: (MonadDiscord m, MonadIO m) => ChannelId -> m ()
 sendInstanceInfoChan chan = do
     host <- liftIO getHostName
@@ -111,25 +109,42 @@ sendInstanceInfoChan chan = do
                           "Host: "            <> T.pack host <> "\n" <>
                           "Process ID: "      <> T.pack (show pid))
 
+-- | Responds to a message with the instance info
+sendInstanceInfo :: Command DiscordHandler
+sendInstanceInfo
+    = requires devPerms
+    . command "instance" $ \m ->
+        sendInstanceInfoChan $ messageChannel m
+
+-- | Kills the bot and spawns a new process.
+-- The new process is spawned in the same cwd (at least on linux)
 restartOwen :: Command DiscordHandler
 restartOwen
     = requires devPerms
+    . help "Kills the bot and starts a new instance."
     . command "restart" $ \m -> do
         respond m  "Restarting..."
         void $ liftIO $ Process.spawnCommand "owenbot-exe"
         stopDiscord
 
--- | Stops the entire Discord chain.
+-- | Safely kills the bot without restarting it.
+-- Any in-progress actions (file writing) should complete, but may not respond
+-- to their calling message.
 stopOwen :: Command DiscordHandler
 stopOwen
     = requires devPerms
-  . command "stop" $ \m -> do
+    . command "stop" $ \m -> do
         respond m "Stopping..."
         stopDiscord
 
+-- | Updates the git repo, compiles, and installs the new binary.
+-- Must be running in a git repo. This is currently hard-coded, and we don't
+-- check if it's the correct repo. Since this is for testing, it's fine.
 updateOwen :: Command DiscordHandler
 updateOwen
     = requires devPerms
+    . help ("Updates the bot to the latest git commit and compiles."
+        <> "The bot must be running from `~/owenbot-hs`")
     . command "update" $ \m -> do
         respond m "Updating..."
         result <- liftIO update
@@ -137,17 +152,25 @@ updateOwen
             then "Finished update"
             else "Failed to update! Please check the logs"
 
--- DEV COMMANDS
+------- DEV COMMANDS
+-- | Gets the list of developer role IDs
 getDevs :: IO [T.Text]
 getDevs = readSingleColCSV devIDs
 
+-- | Updates the list of developer role IDs
 setDevs :: [T.Text] -> IO ()
 setDevs = writeSingleColCSV devIDs
 
+-- | Allows manipulating the list of dev roles without restarting the bot.
 devs :: Command DiscordHandler
 devs
     = requires devPerms
-    . help "List/add/remove registered developer role IDs"
+    . help ("List/add/remove registered developer role IDs:\n"
+        <> "`:devs` alone lists all developer roles.\n"
+        <> "`:devs add <roleID>` sets a given role ID to dev status.\n"
+        <> "`:devs remove <roleID>` removes dev status from a given role ID.\n"
+        <> "To get role IDs, enable Developer Mode in Discord options "
+        <> "and right-click a role.")
     . command "devs" $ \m maybeActionValue -> do
         contents <- liftIO getDevs
         case maybeActionValue :: Maybe (T.Text, RoleId) of
@@ -162,36 +185,26 @@ devs
                 respond m "Removed!"
             Just _ -> respond m "Usage: `:devs {add|remove} <roleId>"
 
-statusRE :: T.Text
-statusRE = "(online|idle|dnd|invisible) "
-           <> "(playing|streaming|competing|listening to) "
-           <> "(.*)"
-
 -- | This can't be polymoprhic because updateStatus requires gateway specific
 -- things.
 setStatus :: Command DiscordHandler
 setStatus
-    = command "status"
+    = requires devPerms
+    . command "status"
     $ \msg newStatus newType (Remaining newName) -> do
         updateStatus newStatus newType newName
         liftIO $ editStatusFile newStatus newType newName
         respond msg
             "Status updated :) Keep in mind it may take up to a minute for your client to refresh."
 
-someComplexThing :: (MonadDiscord m) => Command m
-someComplexThing
-    = command "complex"
-    $ \msg words -> do
-        respond msg $
-            "Length: " <> (T.pack . show . length) words <> "\n"
-            <> "Caught items: \n" <> T.intercalate "\n" words
-
-
+------ LOCKDOWN COMMANDS
 data Lock = Lockdown | Unlock deriving (Show, Eq)
 
+-- | Locks @everyone-level posting permissions in a given channel
 lockdown :: Command DiscordHandler
 lockdown
     = requires modPerms
+    . help "`:lockdown` locks posting permissions for the `everyone` role in this channel."
     . command "lockdown" $ \m -> do
         let chan = messageChannel m
         channel <- getChannel (messageChannel m)
@@ -203,9 +216,11 @@ lockdown
 
             _ -> respond m $ owoify "Channel is not a valid Channel"
 
+-- | Unlocks a locked channel.
 unlock :: Command DiscordHandler
 unlock
   = requires modPerms
+  . help "`:unlock` reverses a `:lockdown`."
   . command "unlock" $ \m -> do
       let chan = messageChannel m
       channel <- getChannel chan
@@ -227,11 +242,12 @@ lockdownChan chan guild b = do
                             }
     editChannelPermissions chan guild swapPermOpts
 
-
---https://discordapi.com/permissions.html#2251673153
+-- | Locks every channel in a Guild
+-- https://discordapi.com/permissions.html#2251673153
 unlockAll :: Command DiscordHandler
 unlockAll
     = requires modPerms
+    . help "`:unlock` for every channel"
     . command "unlockAll" $ \m -> do
         let opts = ModifyGuildRoleOpts Nothing (Just 2251673153) Nothing Nothing Nothing
 
@@ -239,14 +255,15 @@ unlockAll
         modifyGuildRole g g opts
         respond m "unlocked"
 
+-- | Unlocks every channel.
 -- https://discordapi.com/permissions.html#2251671105
 lockAll :: Command DiscordHandler
 lockAll
     = requires modPerms
+    . help "`:lockdown` for every channel"
     . command "lockAll" $ \m -> do
         let opts = ModifyGuildRoleOpts Nothing (Just 2251671105) Nothing Nothing Nothing
 
         let g = fromJust $ messageGuild m
         modifyGuildRole g g opts
         respond m "locked"
-
