84db19d43dbb7f9667c4698fb1046c7db5ae749e
Yuto Takano <moa17stock@gmail.com>
2021-10-16 21:18:31 +0100

    Add help msgs to Academic.hs

--- src/receivers/Academic.hs
+++ src/receivers/Academic.hs
@@ -47,74 +47,94 @@ padZeroes newLen digits = replicate (newLen - length digits) '0' <> digits
 
 -- | Theorem.
 theorem :: (MonadDiscord m, MonadIO m) => Command m
-theorem = alias "thm" $ command "theorem" $ \m subject number -> do
-    case (subject :: T.Text) of
-        "ila" -> case number of
+theorem =
+    alias "thm"
+        . help "Call an ILA theorem.\nUsage: `:theorem <theorem number>`"
+        . command "theorem"
+        $ \m number ->
+    -- When some subject in the future needs to be added (so far we have had none),
+    -- then, add a `subject` argument: `:theorem <subject> <theorem number>`
+                       case number of
             x@TwoDotSeparated{} -> do
                 let path = "ila/theorems/" <> show x <> ".png"
                 let name = show x <> ".png"
                 respondAsset m ("Theorem " <> T.pack name) path
             _ -> respond m "ILA theorems have the format: XX.YY.ZZ!"
-        _ -> respond m "No theorems found for subject!"
 
 -- | Definition.
 definition :: (MonadDiscord m, MonadIO m) => Command m
-definition = alias "def" $ command "definition" $ \m subject number -> do
-    case (subject :: T.Text) of
-        "ila" -> case number of
-            x@OneDotSeparated{} -> do
-                let path = "ila/definitions/" <> show x <> ".png"
-                let name = show x <> ".png"
-                respondAsset m ("Definition " <> T.pack name) path
-            _ -> respond m "ILA definitions have the format: XX.YY!"
-
-        _ -> respond m "No definitions found for subject!"
+definition =
+    alias "def"
+        . help "Call an ILA definition.\nUsage: `:definition <def number>`"
+        . command "definition"
+        $ \m number -> do
+            case number of
+                x@OneDotSeparated{} -> do
+                    let path = "ila/definitions/" <> show x <> ".png"
+                    let name = show x <> ".png"
+                    respondAsset m ("Definition " <> T.pack name) path
+                _ -> respond m "ILA definitions have the format: XX.YY!"
 
 -- | Lemma.
 lemma :: (MonadDiscord m, MonadIO m) => Command m
-lemma = alias "lem" $ command "lemma" $ \m subject number -> do
-    case (subject :: T.Text) of
-        "ila" -> case number of
-            x@TwoDotSeparated{} -> do
-                let path = "ila/lemmas/" <> show x <> ".png"
-                let name = show x <> ".png"
-                respondAsset m ("Lemma " <> T.pack name) path
-            _ -> respond m "ILA lemmas have the format: XX.YY.ZZ!"
-        _ -> respond m "No lemmas found for subject!"
+lemma =
+    alias "lem"
+        . help "Call an ILA lemma.\nUsage: `:lemma <lemma number>`"
+        . command "lemma"
+        $ \m number -> do
+            case number of
+                x@TwoDotSeparated{} -> do
+                    let path = "ila/lemmas/" <> show x <> ".png"
+                    let name = show x <> ".png"
+                    respondAsset m ("Lemma " <> T.pack name) path
+                _ -> respond m "ILA lemmas have the format: XX.YY.ZZ!"
 
 -- | Textbook.
 textbook :: (MonadDiscord m, MonadIO m) => Command m
-textbook = alias "tb" $ command "textbook" $ \m subject -> case () of
-    _ | subject `elem` ["i1a", "inf1a"] ->
-        respondAsset m "the-holy-bible-2.png" "textbooks/i1a-textbook.pdf"
+textbook =
+    alias "tb"
+        . help
+            (  "Find the online textbooks for various subjects.\n"
+            <> "Usage: `:textbook <subject>`"
+            )
+        . command "textbook"
+        $ \m subject -> case () of
+            _ | subject `elem` ["i1a", "inf1a"] ->
+                respondAsset m "the-holy-bible-2.png" "textbooks/i1a-textbook.pdf"
 
-    _ | subject `elem` ["calc", "cap"] ->
-        respond m
-            $ "The textbook can be found here:\n"
-            <> "||http://gen.lib.rus.ec/book/index.php?md5=13ecb7a2ed943dcb4a302080d2d8e6ea||"
+            _ | subject `elem` ["calc", "cap"] ->
+                respond m
+                    $ "The textbook can be found here:\n"
+                    <> "||http://gen.lib.rus.ec/book/index.php?md5=13ecb7a2ed943dcb4a302080d2d8e6ea||"
 
-    _ | subject == "ila" ->
-        respondAsset m "ila-textbook.pdf" "textbooks/ila-textbook.pdf"
+            _ | subject == "ila" ->
+                respondAsset m "ila-textbook.pdf" "textbooks/ila-textbook.pdf"
 
-    _ | subject == "dmp" ->
-        respond m
-            $ "The textbook can be found here:\n"
-            <> "||https://gen.lib.rus.ec/book/index.php?md5=45624cd7153af9bd024f64305e8b7be0||"
+            _ | subject == "dmp" ->
+                respond m
+                    $ "The textbook can be found here:\n"
+                    <> "||https://gen.lib.rus.ec/book/index.php?md5=45624cd7153af9bd024f64305e8b7be0||"
 
-    _ | subject == "inf2c" ->
-        respond m
-            $ "Someone has posted the PDF here:\n"
-            <> "https://discord.com/channels/755798054455738489/887640828128952330/889898467852427345\n"
-            <> "And the EPUB here:\n"
-            <> "https://discord.com/channels/755798054455738489/887640828128952330/889898536852934676"
-    _ -> respond m $ "No textbook registered for `" <> subject <> "`!"
+            _ | subject == "inf2c" ->
+                respond m
+                    $ "Someone has posted the PDF here:\n"
+                    <> "https://discord.com/channels/755798054455738489/887640828128952330/889898467852427345\n"
+                    <> "And the EPUB here:\n"
+                    <> "https://discord.com/channels/755798054455738489/887640828128952330/889898536852934676"
+            _ -> respond m $ "No textbook registered for `" <> subject <> "`!"
 
 -- | Syllogisms.
 syllogisms :: (MonadDiscord m, MonadIO m) => Command m
-syllogisms = alias "syl" $ command "syllogisms" $ \m ->
-    respondAsset m "id-smash-aristotle.png" "cl/syllogisms.png"
+syllogisms =
+    alias "syl"
+        . help "See the list of aristotle syllogisms!"
+        . command "syllogisms"
+        $ \m -> respondAsset m "id-smash-aristotle.png" "cl/syllogisms.png"
 
 -- | Booleans.
 booleans :: (MonadDiscord m, MonadIO m) => Command m
-booleans = alias "bool" $ command "booleans" $ \m ->
-    respondAsset m "literally-satan.png" "cl/Bool.png"
+booleans =
+    alias "bool"
+        . help "See the list of boolean algebra rules!"
+        . command "booleans"
+        $ \m -> respondAsset m "literally-satan.png" "cl/Bool.png"
