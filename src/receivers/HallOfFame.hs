b7f1390281749432431e151f060365832592d56a
Yuto Takano <moa17stock@gmail.com>
2021-10-20 16:17:25 +0100

    Do not send a message on every reaction since it's annoying

--- src/receivers/HallOfFame.hs
+++ src/receivers/HallOfFame.hs
@@ -47,9 +47,10 @@ attemptHallOfFame r = case reactionGuildId r of
                 when (check1 && check2 && check3)
                     $ putInHallOfFame fameTable hofChanId m
             _ ->
-                respond m
-                    $  "Hall of Fame has not been set up in this server! "
-                    <> "Use :setFameChan to set a channel ID."
+                -- respond m
+                --     $  "Hall of Fame has not been set up in this server! "
+                --     <> "Use :setFameChan to set a channel ID."
+                pure ()
 
 
 hallOfFameEmotes :: [T.Text]
